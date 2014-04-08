62630dc01b975f5674ad8b526466c9f41bdd2df7
roopesh90 <roopesh90nair@live.com>
2014-04-08 16:50:27 +0530

    corrections. Test successful Base ready

--- sampleTest.py
+++ sampleTest.py
@@ -5,7 +5,7 @@ class TddSamplePython(unittest.TestCase):
     def test_calculator_add(self):
         calc = Calculator()
         result = calc.add(2,2)
-        assertEqual(result, 4)
+        self.assertEquals(result, 4)
     
 if __name__ == '__main__':
     unittest.main()
